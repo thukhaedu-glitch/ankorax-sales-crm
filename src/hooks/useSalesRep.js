2a71eb4feabdfd4ff17cfd0690b71dc718375f18
thukhaedu-glitch <thukha.edu@gmail.com>
2026-06-07 12:45:37 +0400

    Update useSalesRep.js

--- src/hooks/useSalesRep.js
+++ src/hooks/useSalesRep.js
@@ -1,1 +1,23 @@
+import{useState,useEffect}from'react'
+import{auth,db}from'../firebase'
+import{doc,getDoc}from'firebase/firestore'
+import{onAuthStateChanged}from'firebase/auth'
 
+export default function useSalesRep(){
+const[rep,setRep]=useState(null)
+const[loading,setLoading]=useState(true)
+
+useEffect(()=>{
+const unsub=onAuthStateChanged(auth,async user=>{
+if(user){
+const snap=await getDoc(doc(db,'salesReps',user.uid))
+if(snap.exists())setRep({id:snap.id,...snap.data()})
+else setRep({id:user.uid,email:user.email,name:user.email,role:'rep'})
+}else setRep(null)
+setLoading(false)
+})
+return unsub
+},[])
+
+return{rep,loading}
+}
