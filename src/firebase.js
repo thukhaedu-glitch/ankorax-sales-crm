06fd4bbb2a0767f8a3bdc4175385a48339421bea
thukhaedu-glitch <thukha.edu@gmail.com>
2026-06-07 12:40:39 +0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,16 @@
+import{initializeApp}from'firebase/app'
+import{getFirestore}from'firebase/firestore'
+import{getAuth}from'firebase/auth'
 
+const firebaseConfig={
+apiKey:"YOUR_API_KEY",
+authDomain:"invoice-99bdb.firebaseapp.com",
+projectId:"invoice-99bdb",
+storageBucket:"invoice-99bdb.appspot.com",
+messagingSenderId:"YOUR_SENDER_ID",
+appId:"YOUR_APP_ID"
+}
+
+const app=initializeApp(firebaseConfig)
+export const db=getFirestore(app)
+export const auth=getAuth(app)
